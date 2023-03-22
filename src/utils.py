b8088495512abe33a460fd25fa436c28161946ab
Nneji123 <ifeanyinneji777@gmail.com>
2023-03-22 08:38:40 +0100

    fix: made capture face function faster

--- src/utils.py
+++ src/utils.py
@@ -189,18 +189,18 @@ def capture_face():
         if success:
             # detect faces
             face_locations = face_recognition.face_locations(frame)
-            # draw bounding boxes around the faces
-            for (top, right, bottom, left) in face_locations:
-                cv2.rectangle(frame, (left, top),
-                              (right, bottom), (0, 0, 255), 2)
-                cv2.rectangle(frame, (left, bottom - 35),
-                              (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                text = "Capture Face!"
-                cv2.putText(frame, text, (left + 6, bottom - 6),
-                            font, 1.0, (255, 255, 255), 1)
+            if len(face_locations) > 0:
+                # draw bounding boxes around the faces
+                for (top, right, bottom, left) in face_locations:
+                    cv2.rectangle(frame, (left, top),
+                                  (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35),
+                                  (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    text = "Capture Face!"
+                    cv2.putText(frame, text, (left + 6, bottom - 6),
+                                font, 1.0, (255, 255, 255), 1)
             try:
-                # Optimization: remove unnecessary flip function
                 ret, buffer = cv2.imencode(".jpg", frame)
                 frame = buffer.tobytes()
                 yield (
